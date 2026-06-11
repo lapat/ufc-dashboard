3b127b969487c120010b7221bc6c40ecd288a6cd
Louis Lapat <louislapat@gmail.com>
2026-06-11 13:29:41 -0500

    Poll every 1 second during live fights

--- recorder.js
+++ recorder.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const API_KEY = process.env.ODDS_API_KEY;
-const POLL_INTERVAL_MS = 5000;
+const POLL_INTERVAL_MS = 1000;
 const IDLE_INTERVAL_MS = 60000;
 const DATA_DIR = path.join(__dirname, 'recorded_fights');
 
